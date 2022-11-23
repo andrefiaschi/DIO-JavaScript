4d684f38b457c1da6adcdeaf54db3727a9aeb958
andrefiaschi <andre.luisgonc@gmail.com>
2022-11-23 09:47:48 -0300

    Primeiro commit

---
+++ aulas/aula1.js
@@ -0,0 +1,10 @@
+/* Programa para calulcar o custo de combustível em uma viagem
+*/
+
+let gasPrice = 5.45;
+let kmPerLiter = 11.5;
+let distance = 250;
+
+let tripCost = (distance/kmPerLiter)*gasPrice;
+
+console.log(tripCost);
